29f37ec505bd6ec71c557dbbe77b880bc4ac5f46
MINzer <90465011+MasterPro3007@users.noreply.github.com>
2022-03-11 22:27:30 +0100

    Add files via upload

--- discord.js.functional/mainITA.py
+++ discord.js.functional/mainITA.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.geometry("980x720")
+window.geometry("1645x920")
 window.title("Discord.js semplifcato")
 window.resizable(True, False)
 window.configure(background="blue")
@@ -21,12 +21,20 @@ def inizi_bot():
      textINit_output = tk.Label(window, text=textINit, bg="blue", font=("Supermercado", 16))
      textINit_output.grid(row=0, column=1)
 
+def inst_dsJS():
+     textInst = "Usa nel terminale il commando: npm i discord.js"
+     textInst_output = tk.Label(window, text=textInst, bg="blue", font=("Supermercado", 16))
+     textInst_output.grid(row=0, column=3)
+     
 text = "Discord.js semplificato di MINzer"
 text = tk.Label(window, text=text, bg="Blue", font=("Comic Sans MS", 16))
-text.grid(row=50, column=50)
+text.grid(row=0, column=0)
 
 init_bot = tk.Button(window, text="Inizializza il bot", bg="Blue", font=("Comic Sans MS",16), command=inizi_bot)
-init_bot.grid(row=0, column=0)
+init_bot.grid(row=0, column=1)
+
+inst_lib = tk.Button(window, text="Installa la libreria discord.js", bg="Blue", font=("Comic Sans MS",16), command=inst_dsJS)
+inst_lib.grid(row=0, column=2)
 
 if __name__ == "__main__":
     window.mainloop()
